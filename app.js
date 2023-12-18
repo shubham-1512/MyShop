09346d5d5d8aeeac4f450fe87174b6a8c397c372
shubham-1512 <90674455+shubham-1512@users.noreply.github.com>
2023-12-18 22:14:14 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -21,9 +21,6 @@ const store = mongoDbStore({
 });
 const csrfProtection = csrf();
 
-const privateKey = fs.readFileSync('server.key');
-const certificate = fs.readFileSync('server.cert');
-
 const fileStorage = multer.diskStorage({
     destination: (req,file,cb) =>{
         cb(null,'images');
